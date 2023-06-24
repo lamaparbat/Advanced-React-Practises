89bc22dd7047d0d31884bef24d9f9fa71725bebb
parbat Lama <parbatlama70@gmail.com>
2023-06-24 14:54:38 +0000

    Update 9 files

--- src/services/HTTP.js
+++ src/services/HTTP.js
@@ -2,11 +2,16 @@ import axios from "axios";
 import { HTTP_METHODS } from "../constants/HTTP.constants";
 
 export const HTTP = (method, endpoint, data, headers = null, fullUrl) => {
-  const config = {};
-
   if (!HTTP_METHODS.includes(method) || (!endpoint && !fullUrl)) return;
-
-  config.url = fullUrl ?? `${process.env.REACT_APP_BASE_URL}${endpoint}`;
+  
+  const url = fullUrl ?? `${process.env.REACT_APP_BASE_URL}${endpoint}`;
+  
+  const config = {
+    method, 
+    url, 
+    data, 
+    headers
+  };
 
   return axios(config);
 };
