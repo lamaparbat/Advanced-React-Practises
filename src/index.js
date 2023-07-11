88107129a304cf5bc72949b9d2732d78cf3f4936
Parbat Lama <lamaparbat70@gmail.com>
2023-07-11 16:59:03 +0545

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import App from "./App";
 import ErrorBoundary from "./component/ErrorBoundary";
+import ReactPortal from "./pages/ReactPortal";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -10,7 +12,12 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <ErrorBoundary>
-      <App />
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/reactPortal" element={<ReactPortal />} />
+        </Routes>
+      </Router>
     </ErrorBoundary>
   </StrictMode>
 );
