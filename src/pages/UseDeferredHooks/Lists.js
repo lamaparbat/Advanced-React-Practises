fab925430dad0c3848875d0bf19d66cf29f8b0a9
parbat Lama <parbatlama70@gmail.com>
2023-10-06 10:08:05 +0000

    Update 4 files

--- src/pages/UseDeferredHooks/Lists.js
+++ src/pages/UseDeferredHooks/Lists.js
@@ -2,6 +2,11 @@ const Lists = ({ comments }) => {
   return (
     <>
       {comments?.map((d, i) => {
+        let count = 0;
+        for (let i = 0; i < 100000; i++) {
+          count += 1;
+        }
+        console.log(count);
         return <li key={i}>{d?.name}</li>;
       })}
     </>
