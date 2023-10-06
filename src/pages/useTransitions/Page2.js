5211190678aa18c77a3f652919f6a36f67550a15
parbat Lama <parbatlama70@gmail.com>
2023-10-06 10:51:36 +0000

    Update Page2.js

--- src/pages/useTransitions/Page2.js
+++ src/pages/useTransitions/Page2.js
@@ -1,5 +1,9 @@
-export default function Page2() {
+import React from "react";
+
+const Page2 = () => {
   for (let i = 0; i < 10000000000; i++) {}
 
   return <h1>Expensive page</h1>;
-}
+};
+
+export default React.memo(Page2);
