019689ba615cea3d09190d4045228d5833e64ffa
Parbat Lama <lamaparbat70@gmail.com>
2023-12-27 07:49:53 +0000

    Update index.jsx and style.css

--- src/pages/ScrollAnimation/index.jsx
+++ src/pages/ScrollAnimation/index.jsx
@@ -3,9 +3,9 @@ import "./style.css";
 const ScrollAnimation = () => {
   return (
     <>
-      <div class="liner"></div>
-      <div class="banner">
-        <div class="content">
+      <div className="liner"></div>
+      <div className="banner">
+        <div className="content">
           <h4>Hello World !!</h4>
           <p>
             Lorem ipsum, placeholder or dummy text used in typesetting and
@@ -16,8 +16,82 @@ const ScrollAnimation = () => {
           </p>
         </div>
       </div>
-      <div class="banner2">
-        <div class="content">
+      <div className="banner2">
+        <div className="content">
+          <h4>CONTAINER 2 !!</h4>
+          <p>
+            Lorem ipsum, placeholder or dummy text used in typesetting and
+            graphic design for previewing layouts. It features scrambled Latin
+            text, which emphasizes the design over content of the layout. It is
+            the standard placeholder text of the printing and publishing
+            industries.
+          </p>
+        </div>
+      </div>
+      <div className="banner3">
+        <img
+          src="https://images.pexels.com/photos/884788/pexels-photo-884788.jpeg?auto=compress&cs=tinysrgb&w=600"
+          width="50%"
+        />
+      </div>
+      <div className="banner4">
+        <div className="cards">
+          <div className="card">HTML</div>
+          <div className="card">CSS</div>
+          <div className="card">JAVASCRIPT</div>
+          <div className="card">NODE JS</div>
+          <div className="card">REACT JS</div>
+          <div className="card">HTML</div>
+          <div className="card">CSS</div>
+          <div className="card">JAVASCRIPT</div>
+          <div className="card">HTML</div>
+          <div className="card">CSS</div>
+          <div className="card">JAVASCRIPT</div>
+          <div className="card">NODE JS</div>
+          <div className="card">REACT JS</div>
+          <div className="card">HTML</div>
+          <div className="card">CSS</div>
+          <div className="card">JAVASCRIPT</div>
+        </div>
+      </div>
+      <div className="banner5">
+        <div className="cards">
+          <div className="card">HTML</div>
+          <div className="card">CSS</div>
+          <div className="card">JAVASCRIPT</div>
+          <div className="card">NODE JS</div>
+          <div className="card">REACT JS</div>
+          <div className="card">HTML</div>
+          <div className="card">CSS</div>
+          <div className="card">JAVASCRIPT</div>
+          <div className="card">HTML</div>
+          <div className="card">CSS</div>
+          <div className="card">JAVASCRIPT</div>
+          <div className="card">NODE JS</div>
+          <div className="card">REACT JS</div>
+          <div className="card">HTML</div>
+          <div className="card">CSS</div>
+          <div className="card">JAVASCRIPT</div>
+        </div>
+      </div>
+      <div className="banner2">
+        <div className="content">
+          <h4>CONTAINER 2 !!</h4>
+          <p>
+            Lorem ipsum, placeholder or dummy text used in typesetting and
+            graphic design for previewing layouts. It features scrambled Latin
+            text, which emphasizes the design over content of the layout. It is
+            the standard placeholder text of the printing and publishing
+            industries.
+          </p>
+        </div>
+      </div>
+      <div className="banner3">
+        <img
+          src="https://images.pexels.com/photos/884788/pexels-photo-884788.jpeg?auto=compress&cs=tinysrgb&w=600"
+          width="50%"
+        />
+        <div className="content">
           <h4>CONTAINER 2 !!</h4>
           <p>
             Lorem ipsum, placeholder or dummy text used in typesetting and
